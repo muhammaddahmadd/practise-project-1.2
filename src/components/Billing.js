eee75ab467ebff4743603e5ce4d03da7c14d86f9
Muhammad <ranaahmad131@gmail.com>
2024-08-31 14:34:51 +0500

    working on billing functionality

--- src/components/Billing.js
+++ src/components/Billing.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 
 function Billing({click}){
     const {name, id, balance} = click;
-    return <form className="form-split-bill">
+    const [bill, setBill] = useState("");
+    const [myExpense, setMyExpense] = useState("");
+    // const [friendExpense, setFriendExpense] = useState()
+    const [sort, setSort] = useState("")
+
+    function handleBillAmount(e){
+        setBill(e.target.value)
+    }
+
+    function handleFormSplit(e){
+        e.preventDefault();
+        
+    }
+
+    return <form className="form-split-bill"  onSubmit={handleFormSplit}>
         <h2>Split bill with {!name? "(Click on any friend)" : name} </h2>
         <p>Total bill:</p>
-        <input type="number"/>
+        <input type="number" value={bill} onChange={handleBillAmount}/>
         <p>Your expense:</p>
         <input type="number" />
         <p>{name}'s expense</p>
